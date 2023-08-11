52fc9c1e01533592910d7cdd1760dbaf5be28759
Anatoliy Kireev <bug.tlp@gmail.com>
2023-08-12 00:04:44 +0300

    Add auto connect to Kafka in auth app

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
-import { join } from 'path';
+import { ClientKafka } from '@nestjs/microservices';
 
 import {
   CommonModule,
+  MESSAGE_BROKER_CLIENT,
   dbConnectionProvider,
   messageBrokerClientProvider,
 } from '../../../libs/common/src';
@@ -23,4 +24,10 @@ import { AuthService } from './auth.service';
   controllers: [AuthController],
   providers: [dbConnectionProvider, messageBrokerClientProvider, AuthService],
 })
-export class AuthModule {}
+export class AuthModule {
+  constructor(@Inject(MESSAGE_BROKER_CLIENT) readonly mbClient: ClientKafka) {}
+
+  async onApplicationBootstrap() {
+    await this.mbClient.connect();
+  }
+}
